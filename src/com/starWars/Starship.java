41c78b0aff56f04d496242281758e41ce4e42fcb
TrueDoughnut <Christopher543sap@gmail.com>
2018-04-13 13:58:56 -0500

    Update Starship.java

--- src/com/starWars/Starship.java
+++ src/com/starWars/Starship.java
@@ -5,6 +5,10 @@ import org.json.JSONObject;
 
 public class Starship extends Instance {
 
+	String model, starshipClass, manufacturer, costInCredits,
+		length, crew, passengers, maxAtmospheringSpeed, hyperdriveRating,
+		mglt, cargoCapacity, consumables; 
+	
     Starship(int id, boolean createdFromJSON) {
         super(id, createdFromJSON);
         setResource("starships/");
@@ -19,12 +23,34 @@ public class Starship extends Instance {
 
     @Override
     void assignValues(JSONObject jsonObject) {
-
+		name = jsonObject.getString("name");
+		model = jsonObject.getString("model"); 
+		starshipClass = jsonObject.getString("starship_class"); 
+		manufacturer = jsonObject.getString("manufacturer"); 
+		costInCredits = jsonObject.getString("cost_in_credits"); 
+		length = jsonObject.getString("length") + "m";
+		crew = jsonObject.getString("crew"); 
+		passengers = jsonObject.getString("passengers"); 
+		maxAtmospheringSpeed = jsonObject.getString("max_atmosphering_speed"); 
+		hyperdriveRating = jsonObject.getString("hyperdrive_rating"); 
+		mglt = jsonObject.getString("MGLT"); 
+		cargoCapacity = jsonObject.getString("cargo_capacity"); 
+		consumables = jsonObject.getString("consumables"); 
+		
+		if(!createdFromJSON){
+			getPeopleArray(jsonObject.getJSONArray("pilots")); 
+			getFilmsArray(jsonObject.getJSONArray("films")); 
+		}
     }
 
     @Override
     public String toString(){
-        return super.toString();
+        String msg = ""; 
+		
+		msg += "name: " + name
+			+ "\nmodel: " + 
+		
+		return msg; 
     }
 
 }
